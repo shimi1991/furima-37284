16bbe1df4f6ce306e0edab9d08c617bb86e6c367
shimi1991 <yuu.9134@gmail.com>
2022-03-27 23:46:14 +0900

    販売手数料、販売利益、エラーメッセージ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
     @articles = Article.order("created_at DESC")
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,12 @@ class Item < ApplicationRecord
 
   validates :item_name,          presence: true
   validates :explanation,        presence: true
-  validates :status_id,          presence: true
-  validates :delivery_charge_id, presence: true
-  validates :prefectures_id,     presence: true
-  validates :days_id,            presence: true
-  validates :category_id,        presence: true
-  validates :price,              presence: true
+  validates :status_id,          numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefectures_id,     numericality: { other_than: 1, message: "can't be blank"}
+  validates :days_id,            numericality: { other_than: 1, message: "can't be blank"}
+  validates :category_id,        numericality: { other_than: 1, message: "can't be blank"}
+  validates :price,              presence: true, inclusion: { in: 300..9_999_999, message: "out of setting range" }, format: { with: /\A[0-9]+\z/, message: "invalid. Input half-width characters"}
   validates :user,               presence: true
   validates :image,              presence: true
 end
